9ff6b3da67737d6c65ff7d436be8145d8305bf2a
Abdessalem2000 <abdslemk251@gmail.com>
2026-01-05 01:27:18 +0100

    fix: disable splitting and clear cache

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          dnd: ['@dnd-kit/core', '@dnd-kit/sortable', '@dnd-kit/utilities'],
-          charts: ['recharts']
-        }
-      }
-    }
-  }
+  base: '/',
 })
